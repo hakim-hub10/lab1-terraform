5e808c18ecd14be7b11a9862db8b294aec13abfc
Abdihakim <hakim.abdi@chasacademy.se>
2026-03-11 13:11:01 +0100

    Lab1 Terraform VM setup

---
+++ main.tf
@@ -0,0 +1,43 @@
+# main.tf
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "${var.student_id}-lab1-vm"
+  machine_type = "e2-micro"
+  zone         = "${var.region}-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 20
+      type  = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {} # Ger VM:en en extern IP
+  }
+
+  metadata_startup_script = file("startup.sh")
+
+  labels = {
+    student = var.student_id
+    course  = "devsecops-2026"
+    lab     = "1"
+  }
+
+  tags = ["lab1", "ssh"]
+}
